57c9f27fd4b1f2cbe72687764db1a4c3388352e3
Bibek Subedi <bsubedi26@gmail.com>
2016-06-06 21:31:18 -0400

    Refracted some code, saves to log.txt as JSON instead of string

--- liri.js
+++ liri.js
@@ -1,3 +1,11 @@
+//Require modules
+var fs = require('fs');
+var spotify = require('spotify');
+var request = require('request');
+var Twitter = require('twitter');
+var key = require('./key.js');
+
+//Variables to retrieve what the user types in the command terminal
 var action = process.argv[2];
 var search = process.argv[3];
 
@@ -20,22 +28,9 @@ switch (action) {
         break;
 }
 
-//function that appends to the log.txt file based on the data passed as argument
-function appendToLog(data) {
-        var fs = require('fs');
-        fs.appendFile('log.txt', data, 'utf8', function(err) {
-        if (err) {
-        return console.log(err);
-        }
-        console.log('Log has been updated!');
-        })
-
-}
-
 //function that searches spotify and logs the response based on user search
 function spotifySearch() {
         console.log("Give it a second...")
-        var spotify = require('spotify');
         if(search == undefined) {
             search = "what's my age again";
         }
@@ -53,17 +48,20 @@ function spotifySearch() {
         console.log('--------------------------------------------------------------');
         console.log('Album: ' + response.tracks.items[0].album.name);
         console.log('--------------------------------------------------------------');
-        var spotifyData = 'Artist(s): '+ response.tracks.items[0].artists[0].name +'\n'+'Song Name: ' + 
-        response.tracks.items[0].name+"\n"+'Preview Link: ' + response.tracks.items[0].preview_url+"\n"+
-        'Album: ' + response.tracks.items[0].album.name+"\n\n";
-        appendToLog(spotifyData);
+
+        var spotifyData = {
+            'Artist(s)': response.tracks.items[0].artists[0].name,
+            'Song Name': response.tracks.items[0].name,
+            'Preview Link': response.tracks.items[0].preview_url,
+            'Album': response.tracks.items[0].album.name
+        }
+            appendToLog(spotifyData)
 });
 }
 
 //function that searches omdbapi and logs the response based on user search
 function movieSearch() {
     console.log("Give it a second...");
-    var request = require('request');
     if(search == undefined) {
         search = 'Mr. Nobody';
     }
@@ -91,45 +89,47 @@ function movieSearch() {
             console.log('--------------------------------------------------------------');
             console.log('Rotten Tomatoes URL: '+ body.tomatoURL);
             console.log('--------------------------------------------------------------');
-            var movieData = 'Title: '+ body.Title +'\n'+'Year Released: '+body.Year+"\n"+'Plot: '+ body.Plot +'\n'+
-            'Countries Released in: '+body.Country+"\n"+'Languages Released in: '+body.Language+"\n"+
-            'Actors: '+ body.Actors +'\n'+'IMDB Rating: '+body.imdbRating+"\n"+
-            'Rotten Tomatoes Rating: '+ body.tomatoRating +'\n'+
-            'Rotten Tomatoes URL: '+ body.tomatoURL+'\n\n';
 
+            var movieData = {
+                'Title': body.Title, 'Year Released': body.Year, 'Plot': body.Plot, 
+                'Countries Released in': body.Country, 'Languages Released in': body.Language,
+                'Actors': body.Actors, 'IMDB Rating': body.imdbRating,
+                'Rotten Tomatoes Rating': body.tomatoRating, 'Rotten Tomatoes URL': body.tomatoURL
+                }
             //Passes the movie data to the appendToLog function which appends the data to the log.txt file
             appendToLog(movieData);
         }
     })
 }
 
-// shows your last 20 tweets and when they were created for a designated screen name
+// function that shows the last 20 tweets based on twitter screen name 
 function tweetSearch() {
     console.log("Give it a second...");
-    var Twitter = require('twitter');
-    var key = require('./key.js');
+    console.log('-----------------------------------------------------------------------');
     var Tweets = new Twitter(key.twitterKeys);
     //Twitter parameters and get method to retrieve the appropriate data
     var params = {screen_name: 'nodejs', count: 20};
     Tweets.get('statuses/user_timeline', params, function(error, tweets, response){
       if (!error) {
         for (var i = 0; i < tweets.length; i++) {
-            console.log('--------------------------------------------------------------');
-            var tweetsDate = tweets[i].created_at;
-            var tweetsText = tweets[i].text;
-            console.log(tweetsDate + " || " + tweetsText);
-            console.log('--------------------------------------------------------------');
+            console.log(tweets[i].created_at + " || " + tweets[i].text);
+            console.log('-----------------------------------------------------------------------');
+            var twitterData = {
+                'Date Created': tweets[i].created_at,
+                'Tweets': tweets[i].text
+            }
+            appendToLog(twitterData);
+
         }
       }
     });
 }
-// Using the fs package in node, the program would take the text inside of random.txt and use it to call the first command with the second part as it's parameter
+// function that takes the text inside of random.txt and uses it to call the first command with the second part as it's parameter
 function whateverYouSay() {
-    var fs = require('fs');
     fs.readFile('random.txt', 'utf8', function(error, data) {
         //data is string type so split method is used to turn data into an array
         var splitted = data.split(",");
-        //If statement that passes the data into the appropriate search function
+        //Passes the splitted array into the appropriate search function
         if (splitted[0] == 'spotify-this-song') {
             spotifySearch(splitted[1]);
         }
@@ -137,4 +137,14 @@ function whateverYouSay() {
             movieSearch(splitted[1]);
         }
     })
+}
+
+//function that appends to the log.txt file based on the data passed as argument
+function appendToLog(data) {
+        fs.appendFile('log.txt', JSON.stringify(data, null, '\t'), 'utf8', function(error, response) {
+        if (error) {
+        return console.log(error);
+        }
+        console.log('Log has been updated!');
+        })
 }
